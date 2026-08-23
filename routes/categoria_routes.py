d26700369cc7194528e13c1e8ad1ef188254e358
Saulo Oliveira <saulooliveiram358@gmail.com>
2026-08-22 22:30:24 -0300

    feat: implementa services, repositories e DTOs de Empresa e Categoria

--- routes/categoria_routes.py
+++ routes/categoria_routes.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from repositories.categoria_repository import CategoriaRepository
+from services.categoria_service import CategoriaService
+from dependecies import pegar_session
+
+from dtos.CategoriaDto import CategoriaDto
 
 categoria_router = APIRouter(prefix="/categorias", tags=["categorias"])
 
 
-@categoria_router.get("/")
-async def get_categorias():
-    return {"message": "Categorias encontradas com sucesso!"}
+@categoria_router.post("/criar")
+async def criar_categoria(categoriaDto: CategoriaDto, session : Session = Depends(pegar_session)):
+   categoria_repo = CategoriaRepository(session)
+   categoria_service = CategoriaService(categoria_repo)
+   return categoria_service.criar_categoria(categoriaDto)
--- services/categoria_service.py
+++ services/categoria_service.py
@@ -1,1 +1,24 @@
 # Serviço de Categoria
+from fastapi import HTTPException
+from repositories.categoria_repository import CategoriaRepository
+from dtos.CategoriaDto import CategoriaDto
+from models import Categoria
+
+
+class CategoriaService:
+
+    def __init__(self, categoria_repo: CategoriaRepository):
+        self.categoria_repo = categoria_repo
+
+        
+    def criar_categoria(self, CategoriaDto : CategoriaDto):
+        categoria = self.categoria_repo.buscar_por_nome(CategoriaDto.nome)
+        if categoria:
+            raise HTTPException(status_code=400, detail="Já existe uma categoria cadastrada com este nome!")
+
+        nova_categoria = Categoria(
+            nome=CategoriaDto.nome
+        )
+        
+        self.categoria_repo.salvar(nova_categoria)
+        return {"message" : f"Categoria cadastrada com sucesso! {CategoriaDto.nome}"}
