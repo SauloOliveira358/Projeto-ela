02f50fec1e5155d2decd96d2b7e5c6bea4d01c2e
Saulo Oliveira <saulooliveiram358@gmail.com>
2026-08-24 07:19:52 -0300

    Feat: Criação do DTO Serviço router e repository basico de criação

--- routes/servico_routes.py
+++ routes/servico_routes.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from dtos.ServicosDto import ServicosDto
+from sqlalchemy.orm import Session
+from dependecies import pegar_session
+from repositories.servico_repository import ServicoRepository
+from services.servico_service import ServicoService
 
 servico_router = APIRouter(prefix="/servicos", tags=["servicos"])
 
 
-@servico_router.get("/")
-async def get_servicos():
-    return {"message": "Serviços encontrados com sucesso!"}
+@servico_router.post("/criar")
+async def criar_servico(servicoDto: ServicosDto, session: Session = Depends(pegar_session)):
+    servico_repo = ServicoRepository(session)
+    servico_service = ServicoService(servico_repo)
+    return servico_service.criar_servico(servicoDto)
+    
+    
